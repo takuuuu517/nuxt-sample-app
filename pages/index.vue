6982bcbb0e41487021b70a16ed1db5f7da044bad
Masahiro <sas.surfer0jonny@gmail.com>
2020-06-10 16:56:18 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,29 @@
 <template>
   <v-layout>
     <v-flex>
+      <br>
+      <h2>メッセージ一覧</h2>
       <message-card 
         v-for="message in displayMessages"
-        v-bind:message="message"
-        v-bind:isThread=false
-        v-bind:key="message.ts"
+        :message="message"
+        :isThread=false
+        :key="message.ts"
         @showThread="showThread($event)"
       ></message-card>
+
+      <br>
+      <h2>メッセージを投稿する</h2>
+      <form>
+        <v-textarea
+          outlined
+          label="メッセージを入力してください"
+          v-model="messageForPost"
+        ></v-textarea>
+      </form>
+
+      <div>
+        <v-btn large color="primary" v-on:click="postMessage">SEND</v-btn>
+      </div>
 
       <v-pagination
         v-model="page"
@@ -25,10 +41,25 @@
       <br>
       <message-card 
         v-for="message in threadMessages"
-        v-bind:message="message"
-        v-bind:isThread=true
-        v-bind:key="message.ts"
+        :message="message"
+        :isThread=true
+        :key="message.ts"
       ></message-card>
+
+      <br>
+      <h3>返信する</h3>
+      <form>
+        <v-textarea
+          outlined
+          label="メッセージを入力してください"
+          v-model="messageForPost"
+        ></v-textarea>
+      </form>
+
+      <div>
+        <v-btn large color="primary" v-on:click="postMessage">SEND</v-btn>
+      </div>
+
     </v-flex>
   </v-layout>
 </template>
@@ -53,6 +84,8 @@ export default {
         pageLength: res.data.messages.length / pageSize,
         threadShow: false,
         threadMessages: [],
+        messageForPost: "",
+        threadTs: null
       }
     })
   },
@@ -63,17 +96,41 @@ export default {
       this.closeThread();
       window.scrollTo(0,0)
     },
+    convertParamObjIntoQueryString(params) {
+      const ret = [];
+      for (let param in params)
+        ret.push(encodeURIComponent(param) + "=" + encodeURIComponent(params[param]))
+      return ret.join("&");
+    },
     showThread(ts) {
       this.$axios.get(
         `https://slack.com/api/conversations.replies?token=${process.env.SLACK_API_TOKEN}&channel=${process.env.CHANNEL_ID}&ts=${ts}`
       ).then((res) => {
         this.threadMessages = res.data.messages;
         this.threadShow = true;
+        this.threadTs = ts;
       })
     },
     closeThread() {
       this.threadMessages = [];
+      this.threadTs = null;
       this.threadShow = false;
+    },
+
+    postMessage() {
+      let params = {
+        "token": process.env.SLACK_API_TOKEN,
+        "channel": process.env.CHANNEL_ID,
+        "text": this.messageForPost,
+      }
+      if (this.threadTs !== null) {
+        params["thread_ts"] = this.threadTs;
+      }
+
+      this.$axios.post(
+        `https://slack.com/api/chat.postMessage?${this.convertParamObjIntoQueryString(params)}`
+      ).then((res) => {console.log(res)})
+      this.messageForPost = "";
     }
   }
 }
