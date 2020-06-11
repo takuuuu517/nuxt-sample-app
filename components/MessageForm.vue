585e2b0437dd30bc36aa04310f9c9e980a232cb7
Masahiro <sas.surfer0jonny@gmail.com>
2020-06-11 17:27:23 +0900

    メッセージポスト部分のコンポーネント化

---
+++ components/MessageForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <form>
+      <v-textarea
+        v-model="messageForPost"
+        outlined
+        label="メッセージを入力してください"
+      />
+    </form>
+
+    <div>
+      <v-btn large color="primary" @click="postMessage">
+        SEND
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["thread-ts"],
+    data: function() {
+      return {
+        "messageForPost": "",
+      };
+    },
+    methods: {
+      convertParamObjIntoQueryString(params) {
+        const ret = []
+        for (const param in params)
+          ret.push(
+            encodeURIComponent(param) + "=" + encodeURIComponent(params[param])
+          )
+        return ret.join("&");
+      },
+      postMessage() {
+        let params = {
+          token: process.env.SLACK_API_TOKEN,
+          channel: process.env.CHANNEL_ID,
+          text: this.messageForPost,
+        }
+        if (this.threadTs !== undefined) {
+          params.thread_ts = this.threadTs
+        }
+
+        this.$axios.post(
+          `https://slack.com/api/chat.postMessage?${this.convertParamObjIntoQueryString(
+            params
+          )}`
+        )
+        this.messageForPost = ""
+        this.$emit("message-posted");
+      },
+    },
+  }
+</script>
