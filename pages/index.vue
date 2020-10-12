a89af187e6f9406411f35400f082ce55bd512294
takuuuu517 <takusakiakwa@gmail.com>
2020-10-12 16:46:13 +0900

    applied self-closing

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
         v-bind:isThread=false
         v-bind:key="message.ts"
         @showThread="showThread($event)"
-      ></message-card>
+      />
       <v-form ref="form">
         <v-textarea
           v-model="sendingMessage"
@@ -15,14 +15,14 @@
           :rules="rules"
           label="投稿メッセージ"
           placeholder="投稿文書、URL"
-        ></v-textarea>
+        />
         <v-btn @click="submit">submit</v-btn>
       </v-form>
       <v-pagination
         v-model="page"
         :length="pageLength"
         @input = "selectPage"
-      ></v-pagination>
+      />
     </v-flex>
     <v-flex class="thread-column" v-show="threadShow">
       <h3 class="center">スレッド</h3>
@@ -37,7 +37,7 @@
         v-bind:message="message"
         v-bind:isThread=true
         v-bind:key="message.ts"
-      ></message-card>
+      />
     </v-flex>
   </v-layout>
 </template>
